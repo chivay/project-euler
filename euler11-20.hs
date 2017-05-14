c07c14218a6b83f71e8d57a4e1772562547a5533
Hubert Jasudowicz <hubert.jasudowicz@gmail.com>
2017-05-14 23:06:24 +0200

    Add problem 18

--- euler11-20.hs
+++ euler11-20.hs
@@ -209,6 +209,30 @@ problem17 = sum [ (length . spell) n | n <- [1..1000]]
                                                            else "and" ++ spell mod
             | otherwise = let (tens, mod) = divMod n 10
                           in spell (n - mod) ++ (if mod == 0
-                                                 then "h"
+                                                 then ""
                                                  else spell mod)
 
+problem18 = solveMax (0, 0)
+    where gridStr = "75 \n \
+                   \ 95 64 \n \
+                   \ 17 47 82 \n \
+                   \ 18 35 87 10 \n \
+                   \ 20 04 82 47 65 \n \
+                   \ 19 01 23 75 03 34 \n \
+                   \ 88 02 77 73 07 63 67 \n \
+                   \ 99 65 04 28 06 16 70 92 \n \
+                   \ 41 41 26 56 83 40 80 70 33 \n \
+                   \ 41 48 72 33 47 32 37 16 94 29 \n \
+                   \ 53 71 44 65 25 43 91 52 97 51 14 \n \
+                   \ 70 11 33 28 77 73 17 78 39 68 17 57 \n \
+                   \ 91 71 52 38 17 14 91 43 58 50 27 29 48 \n \
+                   \ 63 66 04 68 89 53 67 30 73 16 69 87 40 31 \n \
+                   \ 04 62 98 27 23 09 70 98 73 93 38 53 60 04 23"
+          parseLine line = map (read::String->Int) (words line)
+          grid = map parseLine $ lines gridStr
+          getValue x y = (grid !! y) !! x
+          maxY = length (lines gridStr) 
+          solveMax (_, y)
+            | y >= maxY = 0
+          solveMax (x,y) = getValue x y + localMax
+              where localMax = max (solveMax (x, y + 1)) (solveMax (x+1, y+1))
